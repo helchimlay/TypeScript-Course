92c72a07a2ffb62581cf9c0c768286ff2b96b3b8
Jakub Olechowski <kubaole2@op.pl>
2022-09-24 13:11:51 +0200

    commit

---
+++ course-lessons/2.basics/app.js
@@ -0,0 +1,6 @@
+var add = function (n1, n2) {
+    return n1 + n2;
+};
+var _a = [3, 5], number1 = _a[0], number2 = _a[1];
+var result = add(number1, number2);
+console.log(result);
---
+++ course-lessons/2.basics/app.ts
@@ -0,0 +1,8 @@
+const add = (n1: number, n2: number) => {
+  return n1 + n2
+}
+
+const [number1, number2] = [3, 5]
+
+const result = add(number1, number2)
+console.log(result)
